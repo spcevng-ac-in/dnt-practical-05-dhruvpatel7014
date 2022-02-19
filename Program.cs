8983755d5ffd4e8d92e6c7123102e26ec770dfee
dhruvpatel7014 <100025899+dhruvpatel7014@users.noreply.github.com>
2022-02-19 17:30:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace practical5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter w = new StreamWriter("test.txt");
+            Console.WriteLine("Enter the data to write in File");
+            string str = Console.ReadLine();
+            w.Write(str);
+            w.Close();
+
+            StreamReader r = new StreamReader("test.txt");
+            string data=r.ReadToEnd();
+            Console.WriteLine($"Content present in file:\n{data}");
+            r.Close();
+            Console.WriteLine("Operation Completed");
+            Console.ReadLine();
+        }
+    }
+}
